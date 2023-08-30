5812a016d2726108c53426da81c3c7440cb0fff6
Timothy Burnsed <143354694+tburnsed@users.noreply.github.com>
2023-08-30 09:30:21 -0400

    Add files via upload

---
+++ PS-Open-Port.ps1
@@ -0,0 +1,12 @@
+﻿#Change for script to run
+$DisplayName = "DISPLAYNAME"
+$portNumber = "PORTNUMBER"
+$Description = "DESCRIPTION" 
+
+write-host "adding rules now..." -ForegroundColor yellow 
+New-NetFirewallRule -DisplayName $DisplayName -Direction Outbound -LocalPort $portNumber -Protocol TCP -Action Allow -Description  $Description
+New-NetFirewallRule -DisplayName $DisplayName -Direction Outbound -LocalPort $portNumber -Protocol UDP -Action Allow  -Description  $Description
+New-NetFirewallRule -DisplayName $DisplayName -Direction Inbound -LocalPort $portNumber -Protocol TCP -Action Allow  -Description  $Description
+New-NetFirewallRule -DisplayName $DisplayName -Direction Inbound -LocalPort $portNumber -Protocol UDP -Action Allow  -Description  $Description
+write-host "Done" -ForegroundColor green 
+pause
